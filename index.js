5bf026850efc7d4e896538374473f3d2bf13db21
helldemons-test <100876094+helldemons-test@users.noreply.github.com>
2022-03-03 21:53:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+```js
 const serverUrl = "https://xeyfqymatfsm.usemoralis.com:2053/server";
 const appId = "FnlubNvWS5J1ayheYTXKdbt1ngQOaxgUH1wtZ4i8";
 Moralis.start({ serverUrl, appId });
@@ -20,8 +21,8 @@ async function mint(){
     const options = {
         type: "erc721",
         receiver: "0xA474cE48300D91334339fb5aDeF99A1B11B1cfe6",
-        contractAddress: "0x9A534628B4062E123cE7Ee2222ec20B86e16Ca8F",
-        tokenId: 3057,
+        contractAddress: "0x60e4d786628fea6478f785a6d7e704777c86a7c6",
+        tokenId: 7469,
       };
       let transaction = await Moralis.transfer(options);
 };
@@ -31,8 +32,8 @@ async function recieve(){
     const options = {
         type: "erc721",
         receiver: "0xA474cE48300D91334339fb5aDeF99A1B11B1cfe6",
-        contractAddress: "0x9A534628B4062E123cE7Ee2222ec20B86e16Ca8F",
-        tokenId: 3120,
+        contractAddress: "0x8a90cab2b38dba80c64b7734e58ee1db38b8992e",
+        tokenId: 9789,
       };
       let transaction = await Moralis.transfer(options);
-};
+};```
